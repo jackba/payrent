5d97914884be4eb795954463b11309773db84393
David French <marsstrum@gmail.com>
2015-01-26 00:59:31 +0000

    Added sendgrid and figaro and revised styling

--- Gemfile
+++ Gemfile
@@ -40,3 +40,4 @@ end
 
 gem 'bootstrap-sass', '~> 3.1.1'
 gem 'devise'
+gem 'figaro', '1.0'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Payrent::Application.config.secret_key_base = 'd608daf5cd59a8b6963ec454e7227eb232a447a6f8ac919bfe304c45f3a840b7967a37d670b86346cc281bd193798013b2ecee2680ca613b0dcc782263a28883'
+Payrent::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
