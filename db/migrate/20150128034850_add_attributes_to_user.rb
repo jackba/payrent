60bd89d5d1237658001722a8a653675d5488f21d
David French <marsstrum@gmail.com>
2015-01-29 01:29:45 +0000

    add additional attributes to User model

---
+++ db/migrate/20150128034850_add_attributes_to_user.rb
@@ -0,0 +1,10 @@
+class AddAttributesToUser < ActiveRecord::Migration
+  def change
+  	add_column :users, :unit_id, :integer
+  	add_cloumn :users, :admin, :boolean, null: false, default: false
+  	add_column :users, :stripe_pub_key, :string
+  	add_column :users, :stripe_secret_key, :string
+
+  	add_index :users, :unit_id
+  end
+end
