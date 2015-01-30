fd6132f12f6fc7cffd86c45e76b9e4e770afea59
David French <marsstrum@gmail.com>
2015-01-30 02:02:45 +0000

    set sign in paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: 'David French', email: 'brookviewonellc@gmail.com', password: 'temp1trial2', password_confirmation: 'temp1trial2' )
